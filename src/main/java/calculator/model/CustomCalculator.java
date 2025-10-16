2d78cd99951e08fbfb6f55b63003a5f455510ee5
Santoragi <woojj1254577@naver.com>
2025-10-16 14:42:19 +0900

    feat(CustomCalculator): 계산 로직 추가
    계산기 클래스에 계산 로직 추가
    - calculate: 계산식에서 구분자를 통해 숫자를 추출 후 모두 더하여 결과 출력

--- src/main/java/calculator/model/CustomCalculator.java
+++ src/main/java/calculator/model/CustomCalculator.java
@@ -2,10 +2,16 @@ package calculator.model;
 
 import calculator.handler.InputHandler;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class CustomCalculator implements Calculator{
 
     private final InputHandler inputHandler;
     private String formula;
+    private Set<String> separators = new HashSet<>(List.of(",", ":"));
 
     public CustomCalculator(InputHandler inputHandler){
         this.inputHandler = inputHandler;
@@ -13,9 +19,49 @@ public class CustomCalculator implements Calculator{
 
     @Override
     public void run() {
-        setFormula(inputHandler.getInput());
 
-        //TODO: 계산 로직 구현
+        setFormula(inputHandler.getInput());
+        addCustomOperator();
+        calculate();
+    }
+
+    private void addCustomOperator(){
+
+        if(formula.startsWith("//")){
+            int index = formula.indexOf("\\n");
+            if(index != -1){
+                for(int i = 2; i < index; i++){
+                    String customOperator = formula.substring(i, i + 1);
+                    separators.add(customOperator);
+                }
+            }
+
+            formula = formula.substring(index + 2);
+        }
+    }
+
+    private void calculate(){
+
+        List<Integer> operands = new ArrayList<>();
+
+        String regex = "[" + separators + "]";
+        String[] numbers = formula.split(regex);
+        for(String number : numbers){
+            if(!number.isEmpty()){
+                operands.add(Integer.parseInt(number));
+            }
+        }
+
+        int result = 0;
+        for(int o : operands){
+            result += o;
+        }
+
+        System.out.println("결과 : " + result);
+    }
+
+    public String getFormula(){
+        return formula;
     }
 
     public void setFormula(String formula) {
